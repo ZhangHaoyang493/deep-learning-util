2ec642baf51ba77e3dd78bd2e69d778d1198ae7a
Haoyang Zhang <841461378@qq.com>
2024-05-31 21:59:36 +0800

    add img-util

--- img_utils.py
+++ img_utils.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import torch
 import torchvision.transforms.functional as F
+import cv2
 
 def read_tensor_img(path: str) -> torch.Tensor:
     """Read an image with PIL.Image and convert it to a Tensor
@@ -29,3 +30,15 @@ def tensor2im(image_tensor, imtype=np.uint8):
     image_numpy = (np.transpose(image_numpy, (1, 2, 0))) * 255.0
     # image_numpy = image_numpy.astype(imtype)
     return image_numpy
+
+def get_diff(img1, img2):
+    img1, img2 = img1.astype(np.float32), img2.astype(np.float32)
+    img_diff = np.abs(img1 - img2)
+    return img_diff.astype(np.uint8)
+
+
+def save_img(path, img):
+    if img.dtype == np.uint8:
+        cv2.imwrite(path, img)
+    elif img.dtype == np.float32 or img.dtype == np.float16 or img.dtype == np.float64:
+        cv2.imwrite(path, img.astype(np.uint8) if img.max() > 1.0 else  (img * 255).astype(np.uint8))
